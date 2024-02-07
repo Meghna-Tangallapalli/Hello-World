679edd95294ddac59f17fd2cbe8d181de35242af
Meghnatangallapalli <meghana.tangallapall>
2024-02-07 12:59:45 +0530

    finished

---
+++ src/main/java/com/subproject/SubProject/App.java
@@ -0,0 +1,13 @@
+package com.subproject.SubProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
